cd6415d0a35c60e587ee64cfbb7f599487231fce
Marinagansi <78782374+Marinagansi@users.noreply.github.com>
2023-01-06 13:07:28 +0545

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:batch_student_starter/helper/objectbox.dart';
 import 'package:batch_student_starter/state/objectbox_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'app/app.dart';
 
@@ -8,7 +9,13 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   //create an object for ObjectBoxInstance
   ObjectBoxState.objectBoxInstance = await ObjectBoxInstance.init();
-  runApp(
-    const MyApp(),
+  //Disbale landscape mode
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then(
+    (value) => runApp(
+      const MyApp(),
+    ),
   );
 }
